583266a5935b0b66025053c4594ed00c222e9a4a
Olawale Akinseye <brainyfarm@gmail.com>
2018-11-04 21:48:02 +0100

    set up boiler plate

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import logger from 'console';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.get('/api/v1', (req, res) => {
+  return res.json({
+    message: "Welcome to BTC Naira API",
+  });
+});
+
+
+app.listen(PORT, () => 
+  logger.log(`Server running on ${PORT}`)
+);
