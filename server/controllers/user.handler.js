c6cd932b576550708219dc51283dd779b6a05e1d
Olawale Akinseye <brainyfarm@gmail.com>
2018-11-06 22:10:49 +0100

    add create user

--- server/controllers/user.handler.js
+++ server/controllers/user.handler.js
@@ -1,18 +1,19 @@
 import _ from 'lodash';
 
-import UserModel from '../models/user.model';
 import * as db from '../helpers/db.operations';
-
+import UserModel from '../models/user.model';
 
 class User {
   static create(request, response) {
-    console.log(request.body);
-    const userData = _.pick(request.body, [
-      'firstname', 'lastname', 'email', 'phone', 'bvn',
-    ]);
+    const userData = _.pick(request.body, ['firstname', 'lastname', 'email', 'phone', 'bvn']);
     return db.createUser(UserModel, userData)
-      .then(instance => response.json(instance))
+      .then(user => {
+        return response.status(201)
+          .json(user);
+      })
+      .catch(error => response.status(400).json({ error }));
   }
+  
 }
 
 export default User;
