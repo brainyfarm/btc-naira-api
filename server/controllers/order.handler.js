11910875f2f251a8125ca40714f11f447baee8aa
Olawale Akinseye <brainyfarm@gmail.com>
2018-11-08 18:29:07 +0100

    add partial create order

--- server/controllers/order.handler.js
+++ server/controllers/order.handler.js
@@ -5,55 +5,54 @@ import Order from '../models/order.model';
 import User from '../models/user.model';
 import Wallet from '../models/wallet.model';
 
+
+import * as db from '../helpers/db.operations';
+
 /**
  * @todo
  * Add controller-level validations
  */
 
+
 class Transaction {
   static async create(request, response) {
     const requestBody = request.body;
-    const { type, user_type, email } = requestBody;
-    
-    const orderPayload = _.pick(requestBody, ['type', 'email', 'asset', 'qty', 'price', ]);
-    const userPayload = user_type === 'new' ?
-      _.pick(requestBody, ['email', 'firstname', 'lastname', 'phone', 'bvn', 'referrer', ]) : null;
-    const accountPayload = type === 'sell' ?
-      _.pick(requestBody, ['account_name', 'account_number', 'bank_name', ]) : null;
-    const walletpayload = type === 'buy' ?
-      _.pick(requestBody, ['wallet_currency', 'wallet_address', ]) : null;
+    const { user_id, account_id, wallet_id, type } = requestBody;
 
+    const userPayload = 
+        _.pick(requestBody, ['email', 'firstname', 'lastname', 'phone', 'bvn', 'referrer', ]);
+    const accountPayload = 
+        _.pick(requestBody, ['account_name', 'account_number', 'bank_name', ]);
+    const walletpayload = 
+        _.pick(requestBody, ['wallet_currency', 'wallet_address', ]);
+    const orderCommon = _.pick(requestBody, ['type', 'asset', 'qty', 'price', ]);
 
-    let userID = await (this.findUserIDByEmail(email));
-    userID = userID ? userID : await (this.registerUserGetID(userPayload));
+      try {
+        const userID = user_id ? user_id : await db.saveRecordGetID(User, userPayload);
+        const accountID = account_id ?
+            account_id : await db.saveRecordGetID(Account, {...accountPayload, user_id: userID});
+        const walletID = wallet_id ?
+            wallet_id : await db.saveRecordGetID(Wallet, {...walletpayload, user_id: userID});
 
+        const orderPayload = type === 'buy' ? {
+          ...orderCommon,
+          user_id: userID,
+          wallet_id: walletID,
+        } : {
+          ...orderCommon,
+          user_id: userID,
+          account_id: accountID,
+        }
+        
+        const orderDetails = await db.createAndReturn(Order, orderPayload);
+        return response.status(201)
+          .json(orderDetails);
+
+      } catch(error) {
+        return response.status(400)
+          .json({ error })
+      }
   }
-
-  findUserIDByEmail(email) {
-    email = email.trim();
-    return new Promise((resolve, reject) => {
-      return User.findOne({ email }, '_id')
-        .then(user => resolve(user._id))
-        .catch(error => reject(error))
-      });
-  }
-
-  registerUserGetID(userData) {
-    return new Promise((resolve, reject) => {
-      const newUser = new User(userData);
-      return newUser.save()
-        .then(user => resolve(user._id))
-        .catch(error => reject(error));
-    });
-
-    // saveTransaction(orderDetails) {
-
-    //   return 
-    // }
-  }
-
-
-
 }
 
 
